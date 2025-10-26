c15231189f3840cd248167910abe039265c808ed
Piotr3009 <piotr.t@skylonconstruction.com>
2025-10-26 19:42:53 +0000

    nie wiem

--- middleware.js
+++ middleware.js
@@ -1,18 +1,11 @@
-import createMiddleware from 'next-intl/middleware';
-import {locales, defaultLocale} from './i18n';
+// TEMPORARY: Middleware disabled for testing
+// This allows the app to work without [locale] folder structure
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: locales,
-
-  // Used when no locale matches
-  defaultLocale: defaultLocale,
-
-  // Don't use a locale prefix for the default locale
-  localePrefix: 'as-needed'
-});
+export default function middleware(request) {
+  // Do nothing - pass through
+  return;
+}
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(en|pl|uk|ro|bg)/:path*']
+  matcher: []
 };
